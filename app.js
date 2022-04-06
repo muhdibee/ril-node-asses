0e8727c3759f01910385c3a9de1852d50735db77
Muhammad <muhdibee@gmail.com>
2022-04-06 03:27:31 +0100

    Initial setup

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const portNum = 3001;
+
+const server = http.createServer((req, res)=> {
+    res.setHeader('Content-type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', "*");
+    res.writeHead(200) //status code HTTP 200 / ok;
+
+
+    let dataObj = {"status":"ok", "message":"Welcome to Muhammad Ibrahim's server", "email":"muhdibee@gmail.com"};
+    let data = JSON.stringify(dataObj);
+    res.end(data);
+});
+
+
+server.listen(portNum, ()=>{
+    console.log('Listening on port: ',portNum);
+});
---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3001;
+
+const server = http.createServer((req, res)=> {
+    console.log(req.headers);
+
+    res.setHeader('Content-type', 'text/html');
+    res.setHeader('Access-Control-Allow-Origin', "*");
+    res.statusCode = 200; //status code HTTP 200 / ok;
+    res.end('<html><body><h1>Hello</h1></body></html>');
+
+});
+
+server.listen(port, ()=>{console.log(`Listening on port ${port}`)});
